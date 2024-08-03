9e4075a4ba8f21f03b94372e0c7069c5d35d9294
Martín <martin.zilber.2008@gmail.com>
2024-08-03 11:44:34 -0300

    MaMeIg - 1.0 // Piedra, papel o tijera - Pre-release 1.0

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Xml.Schema;
 using Microsoft.AspNetCore.Mvc;
 using TP06_Zilbersztein.Models;
 
@@ -15,33 +16,65 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        Informacion.reestablecerValores();
         return View();
     }
-    public IActionResult GuardarNivelSeleccionado(int juego)
+    public IActionResult GuardarJuegoSeleccionado(int juego)
     {
-        Informacion.seleccionarJuego(juego);
+        string ViewRetorno = Informacion.seleccionarJuego(juego);
+        return View("explicacion" + ViewRetorno);
+    }
+    public IActionResult nivelform(){
         return View("nivelform");
     }
-    public IActionResult MaMeIg()
+    public IActionResult GuardarNivel(int nivel)
     {
+        Informacion.EstablecerNivel(nivel);
+        return RedirectToAction(Informacion.juegoString);
+    }
+
+    public IActionResult explicacionmameig()
+    {
+        return View("explicacionmameig");
+    }
+    public IActionResult explicacionpiedrapapeltijera(){
+        return View("explicacionpiedrapapeltijera");
+    }
+    public IActionResult mameig(){
+        int numero = Informacion.calcularNumero(0,Informacion.maximoMaMeIg);
+        ViewBag.numero = numero;
         return View("mameig");
     }
-    public IActionResult MaMeIgSolo()
-    {
-        return View("mameigsolo");
+    public IActionResult validarmameig(string accion){
+        bool esCorrecto = Informacion.validarMaMeIg(accion);
+        if (esCorrecto) ViewBag.Dato = "Felicidades, le pegaste!";
+        else ViewBag.Dato = "No le pegaste";
+        ViewBag.numero = Informacion.DevolverNumero();
+        ViewBag.contador = Informacion.racha;
+        return View("mameig");
     }
-    public IActionResult nivelmameig()
-    {
-        return View("nivelmameig");
+    public IActionResult piedrapapeltijera(){
+        return View("piedrapapeltijera");
     }
-    public IActionResult GuardarNivel(int numJuego, string nivel)
-    {
-        Informacion.EstablecerNivel(numJuego, nivel);
-        return View("");
+    public IActionResult procesarPPT(string jugada){
+        string jugadaBot = Informacion.ProcesarPPT(jugada);
+        ViewBag.contador = Informacion.racha;
+        ViewBag.jugada = $"/images/{jugada.ToLower()}.png";
+        ViewBag.jugadaBot =  $"/images/{jugadaBot.ToLower()}.png";
+        return View("piedrapapeltijera");
     }
+
+
+
+
+
+
+
+
 
     public IActionResult Privacy()
     {
+        ViewBag.NumeroJuego = Informacion.juegoSeleccionado;
         return View();
     }
 
--- Models/Informacion.cs
+++ Models/Informacion.cs
@@ -1,22 +1,86 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace TP06_Zilbersztein.Models;
 static public class Informacion
 {
-    static public int numeroMaMeIg{get;set;}
-    static private int juegoSeleccionado{get;set;}
-    static private string[] juegos{get;set;}=  {"Piedra, papel o tijera", "MAMEIG", "Ahorcado", "El rosco", "Ta-te-ti", ""};
+    static private int minimoMaMeIg { get; set; } = 0;
+    static public int maximoMaMeIg { get; set; }
+    static private int? primerNumeroMaMeIg { get; set; }
+    static private int? segundoNumeroMaMeIg { get; set; }
+    static public int racha { get; set; } = 0;
+    static private int cantLetrasAhorcado { get; set; }
+    static public int? juegoSeleccionado { get; set; }
+    static public string juegoString { get; set; }
+    static private string[] juegos { get; set; } = { "piedrapapeltijera", "mameig", "Ahorcado", "rosco", "Tateti", "" };
+    static private int minimoPPT { get; set; } = 0;
+    static private int maximoPPT { get; set; } = 2;
+    static private string[] jugadasPPT{get;set;} = {"piedra", "papel", "tijera"};
 
-    static public void seleccionarJuego(int juego){
-        juegoSeleccionado = juego;
-    } 
-    static public void EstablecerNivel(int numeroJuego, string nivel){
-        if (numeroJuego == 1)
-        {
-
-        }
+    static public void reestablecerValores()
+    {
+        juegoString = null;
+        juegoSeleccionado = null;
+        racha = 0;
     }
-    static public int calcularNumero(string nivel){
+    static public string seleccionarJuego(int juego)
+    {
+        juegoSeleccionado = juego;
+        juegoString = juegos[(int)juegoSeleccionado - 1];
+        return juegoString;
+    }
+    static public void EstablecerNivel(int nivel)
+    {
+        if (juegoSeleccionado == 2)
+        {
+            if (nivel == 1) maximoMaMeIg = 20;
+            else if (nivel == 2) maximoMaMeIg = 50;
+            else maximoMaMeIg = 100;
+        }
+        else if (juegoSeleccionado == 3)
+        {
+            if (nivel == 1) cantLetrasAhorcado = 5;
+            else if (nivel == 2) cantLetrasAhorcado = 10;
+            else cantLetrasAhorcado = 20;
+        }
+
+    }
+    static public int calcularNumero(int minimo, int maximo)
+    {
         Random R = new Random();
-        int numero = R.Next(1,100);
-        return 0;
+        int numero;
+        numero = R.Next(minimo, maximo);
+        if (primerNumeroMaMeIg == null)
+            primerNumeroMaMeIg = numero;
+        return numero;
+    }
+    static public int DevolverNumero()
+    {
+        return (int)primerNumeroMaMeIg;
+    }
+    static public bool validarMaMeIg(string accion)
+    {
+        bool esCorrecto = false;
+        accion = accion.ToLower();
+        segundoNumeroMaMeIg = calcularNumero(minimoMaMeIg, maximoMaMeIg);
+        if (segundoNumeroMaMeIg > primerNumeroMaMeIg && accion == "mayor" || segundoNumeroMaMeIg < primerNumeroMaMeIg && accion == "menor" || segundoNumeroMaMeIg == primerNumeroMaMeIg && accion == "igual")
+        {
+            esCorrecto = true;
+            racha++;
+        }
+        else
+            racha = 0;
+        primerNumeroMaMeIg = segundoNumeroMaMeIg;
+        return esCorrecto;
+    }
+    static public string ProcesarPPT(string jugada)
+    {
+        string jugadaBot = jugadasPPT[calcularNumero(0,3)];
+        if (jugada == "papel" && jugadaBot == "piedra" || jugada == "tijera" && jugadaBot == "papel" || jugada == "piedra" && jugadaBot == "tijera")
+        {
+            racha++;
+        }
+        else if (jugada != jugadaBot)
+        racha = 0;
+        return jugadaBot;
     }
 }
